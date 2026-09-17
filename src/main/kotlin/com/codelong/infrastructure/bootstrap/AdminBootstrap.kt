332b38e88c84989435eb727e036f01c08f939047
reynanwq <reynanwq@gmail.com>
2026-09-17 15:41:48 -0300

    refactor: remove log de bootstrap desnecessario do admin

--- src/main/kotlin/com/codelong/infrastructure/bootstrap/AdminBootstrap.kt
+++ src/main/kotlin/com/codelong/infrastructure/bootstrap/AdminBootstrap.kt
@@ -5,7 +5,6 @@ import com.codelong.domain.port.UserRepository
 import com.codelong.domain.valueobject.Email
 import com.codelong.domain.valueobject.Username
 import com.codelong.infrastructure.security.SecurityProperties
-import org.slf4j.LoggerFactory
 import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
 import org.springframework.stereotype.Component
@@ -20,8 +19,6 @@ class AdminBootstrap(
     private val userFactory: UserFactory,
     private val properties: SecurityProperties
 ) : ApplicationRunner {
-
-    private val logger = LoggerFactory.getLogger(AdminBootstrap::class.java)
 
     override fun run(args: ApplicationArguments) {
         val username = properties.admin.username.trim()
@@ -39,7 +36,6 @@ class AdminBootstrap(
                 userRepository.save(
                     userFactory.createAdmin(value, Email.of("$username@codelong.local"), password)
                 )
-                logger.info("Admin bootstrap: usuario '{}' criado", username)
             }
     }
 }
